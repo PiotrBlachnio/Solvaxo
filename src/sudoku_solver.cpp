358fe2cf8aceb25368b65185e87f8793c872f31b
Piotr Blachnio <piotr.blachnio@yahoo.com>
2020-10-21 17:22:35 +0200

    Create base structure of SudokuSolver class

---
+++ src/sudoku_solver.cpp
@@ -0,0 +1,13 @@
+#include <vector>
+
+class SudokuSolver {
+    const std::vector<std::vector<int>> board;
+
+    SudokuSolver(std::vector<std::vector<int>> board) {
+        board = board;
+    }
+
+    std::vector<std::vector<int>> solve() {
+        
+    }
+};
