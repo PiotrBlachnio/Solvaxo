78bcf6c07d74fbddc5bdd03c9152e6d3e1df2b86
Piotr Blachnio <piotr.blachnio@yahoo.com>
2020-10-26 06:31:39 +0100

    Add tests to grid-validator.cpp

--- src/grid-validator.cpp
+++ src/grid-validator.cpp
@@ -1,11 +1,11 @@
 #include "grid-validator.h"
 
 bool GridValidator::isGridValid(Square square, Board board) {
-    int gridY = (square.rowIndex + 1) / 3;
-    int gridX = (square.columnIndex + 1) / 3; // TODO: Move it to external method
+    int gridY = square.rowIndex  / 3;
+    int gridX = square.columnIndex / 3; // TODO: Move it to external method
 
-    for(int rowIndex = gridY; rowIndex < gridY + 4; rowIndex++) {
-        for(int columnIndex = gridX; columnIndex < gridX + 4; columnIndex++) {
+    for(int rowIndex = gridY * 3; rowIndex < gridY * 3 + 3; rowIndex++) {
+        for(int columnIndex = gridX * 3; columnIndex < gridX * 3 + 3; columnIndex++) {
             int selectedNumber = board.data[rowIndex][columnIndex];
 
             if(selectedNumber == square.number && (rowIndex != square.rowIndex || columnIndex != square.columnIndex)) return false;
---
+++ tests/grid-validator.test.cpp
@@ -0,0 +1,62 @@
+#include "../lib/catch.h"
+#include "../src/grid-validator.h"
+#include "../src/board.h"
+#include "../src/square.h"
+
+TEST_CASE("Grid validator") {
+    Board board("305420810487901506029056374850793041613208957074065280241309065508670192096512408");
+
+    SECTION("IsGridValid") {
+        SECTION("Should return true when the Grid does not contain a duplicate number") {
+            SECTION("Case 1") {
+                Square square(1, 2, 0);
+
+                bool actual = GridValidator::isGridValid(square, board);
+
+                REQUIRE(actual);
+            }
+
+            SECTION("Case 2") {
+                Square square(6, 5, 8);
+
+                bool actual = GridValidator::isGridValid(square, board);
+
+                REQUIRE(actual);
+            }
+
+            SECTION("Case 3") {
+                Square square(3, 8, 7);
+
+                bool actual = GridValidator::isGridValid(square, board);
+
+                REQUIRE(actual);
+            }
+        }
+
+        SECTION("Should return false when the Grid contains a duplicate number") {
+            SECTION("Case 1") {
+                Square square(5, 2, 0);
+
+                bool actual = GridValidator::isGridValid(square, board);
+
+                REQUIRE_FALSE(actual);
+            }
+
+            SECTION("Case 2") {
+                Square square(4, 5, 8);
+
+                bool actual = GridValidator::isGridValid(square, board);
+
+                REQUIRE_FALSE(actual);
+            }
+
+            SECTION("Case 3") {
+                Square square(1, 8, 7);
+
+                bool actual = GridValidator::isGridValid(square, board);
+
+                REQUIRE_FALSE(actual);
+            }
+        }
+    }
+}
