45ae0650573e98f6e6653c86b066add65da5d11f
Piotr Blachnio <piotr.blachnio@yahoo.com>
2020-10-26 08:27:30 +0100

    Add first test case in sudoku-solver.test.cpp

--- tests/sudoku-solver.test.cpp
+++ tests/sudoku-solver.test.cpp
@@ -1,44 +1,33 @@
-// #include "../src/sudoku-solver.h"
-// #include "test.h"
+#include "../lib/catch.h"
+#include "../src/sudoku-solver.h"
 
-// TEST_CASE("Sudoku solver") {
-//     SECTION("Solve") {
-//         SECTION("Should assign correct numbers to the _board field") {
-//             std::string input = "305420810487901506029056374850793041613208957074065280241309065508670192096512408";
+TEST_CASE("Sudoku solver") {
+    SECTION("Get_Solution_String") {
+        SECTION("Should return string from _board's data") {
+            std::string expected = "305420810487901506029056374850793041613208957074065280241309065508670192096512408";
 
-//             SudokuSolver sudokuSolver(input);
+            SudokuSolver sudokuSolver(expected);
 
-//             sudokuSolver.solve();
+            std::string actual = sudokuSolver.getSolutionString();
 
-//             std::string expected = "365427819487931526129856374852793641613248957974165283241389765538674192796512438";
+            REQUIRE(expected == actual);
+        }
+    }
 
-//             std::string actual = sudokuSolver.getSolutionString();
-//             // std::string actual = "365427819487931526129856374852793641613248957974165283241389765538674192796512438";
+    SECTION("Solve") {
+        SECTION("Should solve the sudoku and assign correct numbers to the _board's data") {
+            SECTION("Case 1") {
+                std::string input = "305420810487901506029056374850793041613208957074065280241309065508670192096512408";
 
-//             REQUIRE(expected == actual);
-//         }
-//     }
-// }
-// TEST_CASE("Sudoku solver") {
-//     SECTION("Board_One") {
-//         std::string input = ;
-//         std::string solvedInput = "365427819487931526129856374852793641613248957974165283241389765538674192796512438";
+                SudokuSolver sudokuSolver(input);
+                sudokuSolver.solve();
 
-//         std::vector<std::vector<int>> board = convertStringToBoard(input);
+                std::string expected = "365427819487931526129856374852793641613248957974165283241389765538674192796512438";
 
-//         SudokuSolver solver(board);
+                std::string actual = sudokuSolver.getSolutionString();
 
-//         std::vector<std::vector<int>> expected = convertStringToBoard(solvedInput); 
-//         std::vector<std::vector<int>> actual = solver.solve();
-
-//         REQUIRE(expected == actual);
-//     }
-// }
-
-// TEST_CASE("SudokuSolver_findEmptySquare") {
-//     SECTION("EmptySquareDoesNotExist_ShouldReturnNULL") {
-//         std::string boardString = "365427819487931526129856374852793641613248957974165283241389765538674192796512438";
-        // std::vector<std::vector<int>> board;
-        // SudokuSolver sudokuSolver(board);
-//     }
-// }
+                REQUIRE(expected == actual);
+            }
+        }
+    }
+}
